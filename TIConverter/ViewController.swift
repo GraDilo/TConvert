d023eabe693e9ebddd04b0feaa51bb22358ea792
Elmir <duck-3@mail.ru>
2024-01-24 19:52:16 +0300

    Initial Commit

---
+++ TIConverter/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TIConverter
+//
+//  Created by Эльмир Юсупов on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var farenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!{
+        didSet{
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
+    
+    @IBAction func sliderValueChange(_ sender: UISlider){
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        
+        let temperatureFarenheit = Int(round(sender.value * 1.8 + 32))
+        farenheitLabel.text = "\(temperatureFarenheit)ºC"
+    }
+    
+
+}
+
